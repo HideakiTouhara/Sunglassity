141b6c0536868b695c19e6854aa7ef64cd8aec5b
HideakiTouhara <hideee6723@gmail.com>
2017-12-06 21:48:58 +0900

    Create collectionView

---
+++ Sunglassity/OptionCell.swift
@@ -0,0 +1,13 @@
+//
+//  OptionCell.swift
+//  Sunglassity
+//
+//  Created by HideakiTouhara on 2017/12/03.
+//  Copyright © 2017年 HideakiTouhara. All rights reserved.
+//
+
+import UIKit
+
+class OptionCell: UICollectionViewCell {
+    
+}
