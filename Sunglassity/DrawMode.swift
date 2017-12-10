110a43450e874ac85ecb6b9bac58fbcfe78aa571
HideakiTouhara <hideee6723@gmail.com>
2017-12-10 18:44:39 +0900

    DrawModeを切り出した

---
+++ Sunglassity/DrawMode.swift
@@ -0,0 +1,9 @@
+//
+//  DrawMode.swift
+//  Sunglassity
+//
+//  Created by HideakiTouhara on 2017/12/10.
+//  Copyright © 2017年 HideakiTouhara. All rights reserved.
+//
+
+import Foundation
--- Sunglassity/ViewController.swift
+++ Sunglassity/ViewController.swift
@@ -46,63 +46,8 @@ class ViewController: UIViewController {
         }
     }
     
-    enum Thickness: String {
-        case thick
-        case medium
-        case thin
-        
-        static let allValues: [Thickness] = [.thick, .medium, .thin]
-        
-        var thickness: CGFloat {
-            switch self {
-            case .thick:
-                return 0.005
-            case .medium:
-                return 0.003
-            case .thin:
-                return 0.001
-            }
-        }
-    }
-    
     var thickness: Thickness = .medium
-    
-    enum Color: String {
-        case red
-        case blue
-        case white
-        
-        static let allValues: [Color] = [.red, .blue, .white]
-        
-        var color: UIColor {
-            switch self {
-            case .red:
-                return UIColor.red
-            case .blue:
-                return UIColor.blue
-            case .white:
-                return UIColor.white
-            }
-        }
-    }
-    
     var color: Color = .white
-    
-    // pictureのモード
-    enum PhotoMode {
-        case trace
-        case put
-    }
-    
-    var photoMode: PhotoMode = .trace
-    
-    // textのモード
-    enum TextMode {
-        case trace
-        case put
-    }
-    
-    var textMode: TextMode = .put
     
     // cell関連
     var previousThicknessNumber = IndexPath(row: 1, section: 0)
