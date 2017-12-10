e75b485f74a0a9e4bfd9978d1892de3a34c09d0d
HideakiTouhara <hideee6723@gmail.com>
2017-12-10 18:46:00 +0900

    drawModeの切り出し

--- Sunglassity/DrawMode.swift
+++ Sunglassity/DrawMode.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+import UIKit
+
+enum Thickness: String {
+    case thick
+    case medium
+    case thin
+    
+    static let allValues: [Thickness] = [.thick, .medium, .thin]
+    
+    var thickness: CGFloat {
+        switch self {
+        case .thick:
+            return 0.005
+        case .medium:
+            return 0.003
+        case .thin:
+            return 0.001
+        }
+    }
+}
+
+enum Color: String {
+    case red
+    case blue
+    case white
+    
+    static let allValues: [Color] = [.red, .blue, .white]
+    
+    var color: UIColor {
+        switch self {
+        case .red:
+            return UIColor.red
+        case .blue:
+            return UIColor.blue
+        case .white:
+            return UIColor.white
+        }
+    }
+}
